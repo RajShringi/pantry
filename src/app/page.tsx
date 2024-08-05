cff1b890fcbfae6edd168823bf94ec4cd4aa9d72
raj shringi <raj24shringi@gmail.com>
2024-08-05 20:28:50 +0530

    add Heading in home page and change metadata title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import Table from "@/components/Table";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center mx-auto gap-6 p-24 max-w-[500px]">
-      <AddItem />
-      <Table />
+    <main className="flex min-h-screen flex-col sm:flex-row items-center justify-center mx-auto gap-6 p-24 max-w-[500px]">
+      <div>
+        <h2 className="text-center py-2 text-lg font-bold tracking-widest">
+          Pantry Tracker
+        </h2>
+        <AddItem />
+        <Table />
+      </div>
     </main>
   );
 }
